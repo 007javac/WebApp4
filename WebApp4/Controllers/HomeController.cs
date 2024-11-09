9db5c70e51d22e70b11d35a95d2b042241700d48
Iurii <yurakaramyshev@yandex.com>
2024-11-09 01:00:37 +0100

    Creating a project

--- WebApp4/Controllers/HomeController.cs
+++ WebApp4/Controllers/HomeController.cs
@@ -4,18 +4,52 @@ using WebApp4.Models;
 
 namespace WebApp4.Controllers
 {
+
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly Dictionary<string, User> _users = new()
         {
-            _logger = logger;
-        }
+            ["bob"] = new User
+            {
+                Name = "Bob",
+                Age = 40,
+                Occupation = "Software Developer",
+                Description = "Опытный разработчик"
+            },
+            ["anna"] = new User
+            {
+                Name = "Anna",
+                Age = 25,
+                Occupation = "UX Designer",
+                Description = "Креативный дизайнер"
+            },
+            ["mike"] = new User
+            {
+                Name = "Mike",
+                Age = 35,
+                Occupation = "Project Manager",
+                Description = "Руководитель проектов с сильными лидерскими качествами"
+            }
+        };
 
-        public IActionResult Index()
+        public IActionResult Index(string name, string color)
         {
-            return View();
+            ViewBag.Color = IsValidColor(color) ? color : "black";
+
+            if (string.IsNullOrEmpty(name))
+            {
+                ViewBag.Message = "Пожалуйста, укажите параметр name в запросе";
+                return View("Message");
+            }
+
+            name = name.ToLower();
+            if (_users.TryGetValue(name, out User user))
+            {
+                return View("UserDetails", user);
+            }
+
+            ViewBag.Message = $"Пользователь с именем {name} не найден";
+            return View("Message");
         }
 
         public IActionResult Privacy()
@@ -23,10 +57,12 @@ namespace WebApp4.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        private bool IsValidColor(string color)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (string.IsNullOrEmpty(color)) return false;
+
+            var validColors = new[] { "red", "blue", "green", "black", "purple", "orange" };
+            return validColors.Contains(color.ToLower());
         }
     }
 }
---
+++ WebApp4/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp4.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Occupation { get; set; }
+        public string Description { get; set; }
+    }
+}
